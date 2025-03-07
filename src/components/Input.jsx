13ba1de5c732fb926bae585e6a61f25b8362d89f
Katiasun <katjatolok3@gmail.com>
2025-03-07 23:39:09 +0100

    Import file NewProject.jsx to App.jsx. Import file Input.jsx to NewProject.jsx.In file Input.jsx write logic

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,1 +1,8 @@
-export default function Input() {}
+export default function Input({ label, textarea, ...props }) {
+  return (
+    <p>
+      <label>{label}</label>
+      {textarea ? <textare {...props} /> : <input {...props} />}
+    </p>
+  );
+}
