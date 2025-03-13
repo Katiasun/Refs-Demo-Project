0915b77abd08632a8fde0dcd6c79516c5ecf57b3
Katiasun <katjatolok3@gmail.com>
2025-03-13 20:48:55 +0100

    Change styles in component Task.jsx for tag h2

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,7 @@ import NewTask from "./NewTask";
 export default function Task() {
   return (
     <section>
-      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <h2 className="text-2xl font-bold text-stone-700 my-4">Tasks</h2>
       <NewTask />
       <p className="text-stone-800 mb-4">This project does not have any tasks yet.</p>
       <ul></ul>
